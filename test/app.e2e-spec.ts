71a1fcfa4b48453a7ad550c43f95d11083e233cf
Darwin John Cruz <darwinjohncruz13@gmail.com>
2023-06-21 13:43:42 +0800

    added user e2e test
    Get me & Edit user

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,6 +4,7 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as pactum from 'pactum';
 import { PrismaService } from '../src/prisma/prisma.service';
 import { AuthDto } from 'src/auth/dto';
+import { EditUserDto } from 'src/user/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -29,7 +30,7 @@ describe('App e2e', () => {
   
   describe('Auth', () => {
     const dto: AuthDto = {
-      email: 'kai@gmail.com',
+      email: 'darwin@gmail.com',
       password: '123'
     }
     describe('Signup', () => {
@@ -95,16 +96,38 @@ describe('App e2e', () => {
           .spec()
           .post('/auth/signin')
           .withBody(dto)
-          .expectStatus(200);
+          .expectStatus(201)
+          .stores('userAt', 'access_token')	
       })
     })
     
   })
 
   describe('User', () => {
-    describe('Get me', () => {})
+    describe('Get me', () => {
+      it('Should get current user', () => {
+        return pactum
+        .spec()
+        .get('/users/me')
+        .withHeaders({ Authorization: 'Bearer $S{userAt}' })
+        .expectStatus(200)
+      })
+    })
 
-    describe('Edit user', () => {})
+    describe('Edit user', () => {
+      it('Should edit user', () => {
+        const dto: EditUserDto = {
+          firstName: 'DarwinJohn',
+          lastName: 'Cruz'
+        }
+        return pactum
+        .spec()
+        .patch('/users')
+        .withHeaders({ Authorization: 'Bearer $S{userAt}' })
+        .withBody(dto)
+        .expectStatus(200);
+      })
+    })
   })
 
 })
